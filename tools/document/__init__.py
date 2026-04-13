61cca1d976c2bf6abbc7a403339d68bf954dc73b
chen4115 <chen4115@sup.com>
2026-04-13 10:35:45 +0800

    Clear repo

--- tools/document/__init__.py
+++
@@ -1,5 +0,0 @@
-from tools.document.file_parser import FileParser
-from tools.document.file_cleaner import DocxCleaner, clean_docx
-from tools.document.docx_report_generator import ReportGenerator
-
-__all__ = ["FileParser", "DocxCleaner", "clean_docx", "ReportGenerator"]
